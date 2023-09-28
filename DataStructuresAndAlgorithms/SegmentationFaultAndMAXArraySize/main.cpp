c008bffcadf9998e51d7b9ee35a57bca8f03a70c
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-09-29 00:06:25 +0500

    The Segmentation problems with Array, segmentation is when you try to access a memory location which is not yours

---
+++ DataStructuresAndAlgorithms/SegmentationFaultAndMAXArraySize/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+
+int main(){
+//    int a[1000000] {};
+    //increasing array size more than 10 power 6 causes segmentation fault and that is the
+    //max size of an array in c++
+    int array [10] {};
+    std::cout<<array[2000];
+    
+    std::cout<<std::endl;
+    return 0;
+}
