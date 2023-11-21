b9f87627d95823a009f926c5bda98caf81faedde
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-11-21 17:35:02 +0500

    Deep Copy, create a new array and copy the data

--- DataStructuresAndAlgorithms/ShallowCopy/main.cpp
+++ DataStructuresAndAlgorithms/ShallowCopy/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<cstring>
+
 using namespace std;
 
 class Student {
@@ -7,7 +9,8 @@ class Student {
 public:
     Student(int age,char *name){
         this->age = age;
-        this->name =name;
+        this->name = new char{strlen(name) + 1};
+        strcpy(this->name,name);
     }
     void display(){
         cout<<name<<" "<<age<<endl;
@@ -17,20 +20,11 @@ int main(){
     char name [] = {"Atif"};
     Student s1(30,name);
     s1.display();
-    //This programs performs a shallow copy of the name which in simple terms can be
-    //called as address from where the data was read and in response changes were made back
-    //their immediatly............
-    /*Both the objects are pointing to the same array and when one object changes something it is 
-     reflected in the other...
-      * 
-      * 
-      * 
-      * 
-      * 
-      * AS SHALLOW COPY BASICALLY MEANS COPY THE ADDRESS ONLY*/
     name[3] = 'e';
+    
     Student s2(20,name);
     s2.display();
+    s1.display();
     
     
     
