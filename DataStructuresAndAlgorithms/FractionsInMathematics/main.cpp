860ac8ea8bc260b04f20a55b68f7b7917a481474
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-12-05 22:53:46 +0500

    Operator Overloading

--- DataStructuresAndAlgorithms/FractionsInMathematics/main.cpp
+++ DataStructuresAndAlgorithms/FractionsInMathematics/main.cpp
@@ -43,10 +43,36 @@ public:
         fnew.simplify();
         return fnew;
     }
+    
+    Fraction operator+(Fraction f2){
+        int lcm = this->numerator * f2.denuminator;
+        int x = lcm/denuminator;
+        int y = lcm/f2.denuminator;
+        int num = x*numerator + y*f2.numerator;
+        
+        //store result in f1
+//        this->numerator = num;
+//        this->denuminator = lcm;
+        Fraction fnew(num,lcm);
+        fnew.simplify();
+        return fnew;
+    }
     void multiply(Fraction const &f2){
         numerator = numerator * f2.numerator;
         denuminator = denuminator*f2.denuminator;
         simplify();
+    }
+    Fraction operator*(Fraction &f2){
+        int n = numerator * f2.numerator;
+        int d = denuminator * f2.denuminator;
+        
+        Fraction fnew(n,d);
+        fnew.simplify();
+        return fnew;
+    }
+    
+    bool operator==(Fraction &f2){
+        return(numerator == f2.numerator && denuminator == f2.denuminator);
     }
     
     void simplify(){
@@ -69,16 +95,26 @@ int main(){
     Fraction f1(10,2);
     Fraction f2(15,4);
     Fraction f3;
-     f3 = f1.add(f2);
+    Fraction f4;
+    
+    f3 = f1.add(f2);
+    f4 = f1 * f2;
+    
+  
      
      f1.print();
      f2.print();
      f3.print();
+     f4.print();
      
+      if(f1 == f1){
+        cout<<"Equal"<<endl;
+    }
+    else
+        cout<<"Not Equal"<<endl;
     
     
-    
-    cout<<f3.getDenuminator()<<f3.getNumerator()<<endl;
+//    cout<<f3.getDenuminator()<<f3.getNumerator()<<endl;
     
     return 0;
 }
