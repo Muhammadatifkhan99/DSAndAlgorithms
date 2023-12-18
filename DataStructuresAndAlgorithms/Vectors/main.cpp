7d86614d504c2ef16e1db611e1b929505c0b08b4
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-12-18 21:30:03 +0500

    Start algorithms section

--- DataStructuresAndAlgorithms/Vectors/main.cpp
+++ DataStructuresAndAlgorithms/Vectors/main.cpp
@@ -20,6 +20,9 @@ int  main(){
         v.push_back(i+1);
         
     }
+    
+    //sorting a vector
+    sort(v2.begin(),v2.end());
 //    for(int i{0};i<v.size();i++){
 //        cout<<v[i]<<endl;
 //    }
